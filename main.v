31780afa2fc35cff069c84cf7299ad21ca7fc395
wgooch2000@gmail.com <wgooch2000@gmail.com>
2021-04-30 23:32:02 -0400

    Began Extrapolating Prompt To Module

--- main.v
+++ main.v
@@ -2,18 +2,16 @@ module main
 
 import term
 import term.ui as tui
-import io
 import os
-import rand
+import prompt as prmpt
 
 struct App {
 mut:
 	tui    &tui.Context = 0
-	prompt &Prompt
+	prompt &prmpt.Prompt
 }
 
 fn event(e &tui.Event, mut app App) {
-	// mut app := &App(x)
 	println(e)
 	match e.typ {
 		.key_down {
@@ -24,9 +22,9 @@ fn event(e &tui.Event, mut app App) {
 				32...126 { // 0-9a-zA-Z
 					app.prompt.input(e.ascii, app.prompt.cursor_line, app.prompt.cursor_column)
 				}
-            .backspace {
+				.backspace {
 					app.prompt.backspace(app.prompt.cursor_line, app.prompt.cursor_column)
-            }
+				}
 				else {}
 			}
 		}
@@ -35,10 +33,10 @@ fn event(e &tui.Event, mut app App) {
 }
 
 fn main() {
-	mut t_lines := load_dict(os.dir(os.executable()) + '/words', 100)
-	paragaph := random_paragraph(t_lines, 10)
+	mut t_lines := prmpt.load_dict(os.dir(os.executable()) + '/words', 100)
+	paragaph := prmpt.random_paragraph(t_lines, 10)
 	mut app := &App{
-		prompt: &Prompt(Quote{
+		prompt: &prmpt.Prompt(prmpt.Quote{
 			lines: paragaph
 		})
 	}
@@ -58,7 +56,10 @@ fn frame(mut app App) {
 	app.tui.clear()
 
 	for i, line in app.prompt.lines {
-		line.print(width / 2, height / 2 + i, mut app)
+		for j in 0 .. line.len {
+			app.tui.draw_text(width / 2 - line.len / 2 + j, height / 2 + i, line.text[j].col_fn(line.text[j].chr.str()))
+		}
+		// line.print(width / 2, height / 2 + i, mut app)
 	}
 
 	app.tui.reset()
@@ -69,108 +70,3 @@ fn frame(mut app App) {
 		height / 2 + app.prompt.cursor_line})
 	term.show_cursor()
 }
-
-fn load_dict(file string, cap int) []string {
-	mut lines := []string{}
-	mut f := os.open(file) or { panic(err) }
-	defer {
-		f.close()
-	}
-	mut r := io.new_buffered_reader(reader: io.make_reader(f), cap: cap)
-	for {
-		l := r.read_line() or { break }
-		lines << l
-	}
-	return lines
-}
-
-fn random_paragraph(dict []string, word_count u32) []LineBuffer {
-	mut paragaph := []LineBuffer{}
-	mut words := 0
-	for words < word_count {
-		mut temp_line := LineBuffer{}
-		for i in 0 .. 4 {
-			temp_line.push_word(dict[rand.int_in_range(0, dict.len)] + if i < 4 {
-				' '
-			} else {
-				''
-			})
-			words += 1
-		}
-		paragaph << temp_line
-	}
-	return paragaph
-}
-
-interface Prompt {
-mut:
-	cursor_line int
-	cursor_column int
-	lines []LineBuffer
-}
-
-fn (mut p Prompt) input(char rune, row int, column int) {
-	if p.lines[row].text[column].chr == char {
-		p.lines[row].text[column].col_fn = term.black
-	} else {
-		p.lines[row].text[column].col_fn = term.red
-	}
-	if p.cursor_column < p.lines[row].len - 1 {
-		p.cursor_column += 1
-	} else {
-		p.cursor_line += 1
-		p.cursor_column = 0
-	}
-}
-
-fn (mut p Prompt) backspace(row int, column int) {
-	p.lines[row].text[column-1].col_fn = term.white
-	p.cursor_column -= 1
-}
-
-struct Quote {
-mut:
-	cursor_line   int
-	cursor_column int
-	lines         []LineBuffer
-	link          Url
-}
-
-struct Url {
-	url  string
-	text string
-}
-
-struct Char {
-mut:
-	col_fn fn (string) string
-	chr    rune
-}
-
-struct LineBuffer {
-mut:
-	len int
-	// Maximum 16 words. 4 is average length of a word,
-	// and an extra rune is allocated for whitespace and
-	// punctuation. 80 = 16 * 5.
-	word_ind int
-	words    [16]byte
-	text     [80]Char
-}
-
-fn (b LineBuffer) print(x int, y int, mut app App) {
-	for i in 0 .. b.len {
-		app.tui.draw_text(x - b.len / 2 + i, y, b.text[i].col_fn(b.text[i].chr.str()))
-	}
-}
-
-fn (mut b LineBuffer) push_word(word string) {
-	b.words[b.word_ind] = byte(word.len)
-	for i in 0 .. word.len {
-		b.text[b.len + i].chr = word[i]
-		b.text[b.len + i].col_fn = term.dim
-	}
-	b.len += word.len
-	b.word_ind += 1
-}
-
---
+++ prompt/prompt.v
@@ -0,0 +1,111 @@
+module prompt
+
+import term
+import io
+import os
+import rand
+
+pub fn load_dict(file string, cap int) []string {
+	mut lines := []string{}
+	mut f := os.open(file) or { panic(err) }
+	defer {
+		f.close()
+	}
+	mut r := io.new_buffered_reader(reader: io.make_reader(f), cap: cap)
+	for {
+		l := r.read_line() or { break }
+		lines << l
+	}
+	return lines
+}
+
+pub fn random_paragraph(dict []string, word_count u32) []LineBuffer {
+	mut paragaph := []LineBuffer{}
+	mut words := 0
+	for words < word_count {
+		mut temp_line := LineBuffer{}
+		for i in 0 .. 4 {
+			temp_line.push_word(dict[rand.int_in_range(0, dict.len)] + if i < 4 {
+				' '
+			} else {
+				''
+			})
+			words += 1
+		}
+		paragaph << temp_line
+	}
+	return paragaph
+}
+
+interface Prompt {
+mut:
+	cursor_line int
+	cursor_column int
+	lines []LineBuffer
+}
+
+pub fn (mut p Prompt) input(char rune, row int, column int) {
+	if p.lines[row].text[column].chr == char {
+		p.lines[row].text[column].col_fn = term.black
+	} else {
+		p.lines[row].text[column].col_fn = term.red
+	}
+	if p.cursor_column < p.lines[row].len - 1 {
+		p.cursor_column += 1
+	} else {
+		p.cursor_line += 1
+		p.cursor_column = 0
+	}
+}
+
+pub fn (mut p Prompt) backspace(row int, column int) {
+	p.lines[row].text[column - 1].col_fn = term.white
+	p.cursor_column -= 1
+}
+
+pub struct Quote {
+mut:
+	cursor_line   int
+	cursor_column int
+	lines         []LineBuffer
+	link          Url
+}
+
+struct Url {
+	url  string
+	text string
+}
+
+struct Char {
+pub mut:
+	col_fn fn (string) string
+	chr    rune
+}
+
+pub struct LineBuffer {
+pub mut:
+	len int
+	// Maximum 16 words. 4 is average length of a word,
+	// and an extra rune is allocated for whitespace and
+	// punctuation. 80 = 16 * 5.
+	word_ind int
+	words    [16]byte
+	text     [80]Char
+}
+
+pub fn (mut b LineBuffer) push_word(word string) {
+	b.words[b.word_ind] = byte(word.len)
+	for i in 0 .. word.len {
+		b.text[b.len + i].chr = word[i]
+		b.text[b.len + i].col_fn = term.dim
+	}
+	b.len += word.len
+	b.word_ind += 1
+}
+
+// pub fn (b LineBuffer) get_rune(x int, y int) Char {
+// return b.text[i].col_fn(b.text[i].chr.str())
+// for i in 0 .. b.len {
+// 	app.tui.draw_text(x - b.len / 2 + i, y, b.text[i].col_fn(b.text[i].chr.str()))
+// }
+// }
